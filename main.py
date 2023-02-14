7c2e16eea27e4623ca0e03f0ae7ccd71b24b4e97
Nguyen Duc Hoang <nguyenduchoang@hoangks5.local>
2023-02-14 15:59:45 +0700

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+{
+    'link_url': "link website nhà đang rao bán",
+    'type_sale': "Cho thuê hay bán"
+    'title': "tên tiêu đề topic",
+    'district': "Thanh Xuân",
+    'address': "Địa chỉ cụ thể nhà",
+    'bio': "Thông tin mô tả, nội dung bài đăng",
+    'real_estate_features': {
+            'type_of_house': "Loại nhà , căn hộ chung cư, nhà riêng, nhà mặt phố",
+            'acreage': "Diện tích m2",
+            'juridical': "Sổ đỏ/Sổ hồng", # Pháp lý , thường thuê nhà thì trường này hay để none
+            'facede': "Mặt tiền bn mét",
+            'interior': "Nội thất",
+            'number_of_floors': "Số tầng",
+            'number_of_toilets': "Số Toilets",
+            'number_of_bedrooms': "Số phòng ngủ",
+            'price': "Giá thuê triệu/tháng",
+            'price/acreage': "Giá tiền/m2"
+            'road_width_in_front_of_house': "Đường trước nhà rộng bn",
+            'the_direction_of_the_house': "Hướng nhà Đông tây nam bắc"
+            .....
+    },
+    'link_map': "GG Map vị trí nhà",
+    'data_submitted': "Ngày đăng bài viết",
+    'link_image': [] , # Ảnh nhà
+}
